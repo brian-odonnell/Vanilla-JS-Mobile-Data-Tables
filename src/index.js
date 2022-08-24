b42dafd3c1b8cd89878e6893c2dd3c492297daa5
Brian O'Donnell <brian.odonnell@hansoninc.com>
2022-08-24 19:16:30 -0400

    Removed files and data not needed from Webpack starting point

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import generateJoke from "./generateJoke";
 import './scss/main.scss';
-import laughing from './assets/laughing.svg';
-
-const laughImg = document.getElementById('laughImg');
-laughImg.src = laughing;
 
 console.log(generateJoke());
